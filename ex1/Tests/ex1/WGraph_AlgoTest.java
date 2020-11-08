f257995b178c59e538b1a860d7b1bf05784cb116
ItaiLash <73061559+ItaiLash@users.noreply.github.com>
2020-11-08 17:57:57 +0200

    init

---
+++ ex1/Tests/ex1/WGraph_AlgoTest.java
@@ -0,0 +1,40 @@
+package ex1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WGraph_AlgoTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getGraph() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void isConnected() {
+    }
+
+    @Test
+    void shortestPathDist() {
+    }
+
+    @Test
+    void shortestPath() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void load() {
+    }
+}
